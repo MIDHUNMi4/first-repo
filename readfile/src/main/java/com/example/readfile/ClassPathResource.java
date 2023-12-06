2a73b70fcdcf52785c8b47f0573136c631f56c57
Midhun M <immi4mid@gmail.com>
2023-12-06 13:51:29 +0530

    Reading from text file

---
+++ readfile/src/main/java/com/example/readfile/ClassPathResource.java
@@ -0,0 +1,4 @@
+package com.example.readfile;
+
+public class ClassPathResource {
+}
---
+++ readfile/src/main/java/com/example/readfile/TextFileReader.java
@@ -0,0 +1,6 @@
+package com.example.readfile;
+
+public class TextFileReader {
+    private String key;
+    private String value;
+}
